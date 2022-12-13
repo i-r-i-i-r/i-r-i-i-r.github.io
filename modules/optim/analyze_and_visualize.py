e295f37fb05d65302af8d45aa5e8f8fdd367e5ed
i-r-i-i-r <ken.iritani.0@gmail.com>
2022-12-14 07:54:19 +0900

    「折れ線回帰」の機能を追加

--- modules/optim/analyze_and_visualize.py
+++ modules/optim/analyze_and_visualize.py
@@ -3,75 +3,26 @@ import pandas as pd
 import os
 import nonlinopt # 非線形最適化
 from plot import make_plot # 作図
+from io_json import read_json
+"""
+import json
+import collections as cl
 
-# 目的関数
-def objfun(param,x,y, gain=200):
-    W      = calc_W(param, x, y, gain)
-    y_calc = calc_y(W, param, x, gain)
-    residual = y_calc - y
-    rmse = np.sqrt((residual*residual).sum()/len(residual))
-    return rmse
+def read_json(file_name:str) -> dict: # jsonファイルの読み込み
+    decoder = json.JSONDecoder(object_pairs_hook=cl.OrderedDict)
+    with open(file_name) as conf_file:
+        data = decoder.decode(conf_file.read())
+    return data
 
-# 制約式
-def consfun(param):
-    cons = []
-    return cons
+config_opt = read_json("C:\\Users\\irita\\Desktop\\development\\git-pages\\i-r-i-i-r.github.io\\modules\\optim\\config_opt.json")
+dataset=pd.read_csv("C:\\Users\\irita\\Desktop\\development\\git-pages\\i-r-i-i-r.github.io\\data\\optim\\example 3.csv")
+"""
 
-# 重回帰分析における入力変数の情報を持つ行列の作成
-def calc_Phi(x, param, gain):
-    s     = sigmoid(x, param, gain)
-    ones_ = np.ones_like(x)
-    Phi = np.transpose(np.vstack([ones_*(1-s), x*(1-s), x*x*(1-s), ones_*s, x*s]))
-    return Phi
-
-# シグモイド関数
-def sigmoid(x, x0, gain):
-    return 1/(1+np.exp(-gain*(x-x0)))
-
-# 線形回帰式による出力変数の計算
-def calc_y(W, param, x, gain):
-    Phi = calc_Phi(x, param, gain)
-    Y = np.dot(Phi, W)
-    return Y
-
-# リッジ回帰における未知係数の計算
-def calc_W(param, x, y, gain, alpha=0.02):
-    Phi   = calc_Phi(x, param, gain)
-    Phi_T = np.transpose(Phi)
-    A = np.linalg.pinv( np.dot(Phi_T, Phi)+alpha*np.eye(Phi.shape[1]) )
-    B = np.dot(A, Phi_T)
-    W = np.dot(B, y)
-    return W
-
-def get_plot(dataset, n_deg, intercept_zero):
-    #
-    """
-    # パラメータの最適化
-    l_param_init = np.linspace(0, 1, 21)
-    fval=1e8
-    for param_init in l_param_init:
-        param_opt_, fval_, constype = nonlinopt(config, param_init, (x_data,y_data), objfun, consfun )
-        if fval>fval_:
-            param_opt = param_opt_
-            fval = fval_
-    W_opt = calc_W(param_opt, x_data, y_data, gain)
-    
-    # フィッティング
-    gain = 200
-    y_fit       = calc_y(W_opt,  param_opt,  x_fit, gain)
-    y_data_pure = calc_y(W_true, param_true, x_fit, gain)
-    
-    # 出力
-    #print("x0: true={:.2f}, opt={:.2f}".format(param_true, param_opt[0]))
-    #print("W_true")
-    #print(W_true)
-    #print("W_opt")
-    #print(W_opt)
-    """
-    x_data = dataset["x"].values
+# 多項式近似
+def polyfit_(x_data, y_data, x_fit, intercept_zero, n_deg):
+    # 計算
     phi_data = np.array([[x_i**i_deg for i_deg in range(n_deg,-1,-1)] for x_i in list(x_data)])
     
-    x_fit = np.linspace(x_data[0], x_data[-1], 51)
     if intercept_zero:
         if min(x_data)>0:
             x_fit = np.linspace(0, x_data[-1], 51)
@@ -79,17 +30,14 @@ def get_plot(dataset, n_deg, intercept_zero):
             x_fit = np.linspace(x_data[0], 0, 51)
     phi_fit = np.array([[x_i**i_deg for i_deg in range(n_deg,-1,-1)] for x_i in list(x_fit) ])
     
-    y_data = dataset["y"].values
     if intercept_zero:
         coef_vec = np.linalg.lstsq(phi_data[:, :-1], y_data, rcond=None)[0]
         y_fit = np.dot(phi_fit[:, :-1], coef_vec)
-        disp_orient = 1
     else:
         coef_vec = np.linalg.lstsq(phi_data, y_data, rcond=None)[0]
         y_fit = np.dot(phi_fit, coef_vec)
-        disp_orient = 0
     
-    #coef_vec = [100,0.02,1,2,3]
+    # 式（表示用）
     coef_disp_vec0 = ["{:+.2e}".format(c) for c in list(coef_vec)]
     coef_disp_vec0 = [c[0]+" "+c[1:].replace("e+00","") for c in coef_disp_vec0]
     coef_disp_vec1 = []
@@ -116,49 +64,110 @@ def get_plot(dataset, n_deg, intercept_zero):
     if formula2[0].split("+")[0]=="":
         formula2[0] = formula2[0][1:]
     
-    
     formula3 = r'$y$ = '+' '.join(formula2)
     if n_deg>=6:
         formula3=formula3.split("$x^{4}$")[0]+r"$x^{4}$"+ "\n"+formula3.split("$x^{4}$")[1]
     if n_deg>=9:
         formula3=formula3.split("$x^{7}$")[0]+r"$x^{7}$"+ "\n"+formula3.split("$x^{7}$")[1]
     
-    """
-    coef_disp_vec0 = ["{:+.2e}".format(c) for c in list(coef_vec)]
-    coef_disp_vec0 = [c[0]+" "+c[1:].replace("e+00","") for c in coef_disp_vec0]
-    coef_disp_vec1 = []
-    for c in coef_disp_vec0:
-        if "e" in c:
-            if "e+01" in c:
-                coef_disp_vec1 += [c.split("e")[0]+"\times 10"]
-            else:
-                coef_disp_vec1 += [c.split("e")[0]+"\times "+"10^{"+str(int(c.split("e")[1]))+"}"]
-        else:
-            coef_disp_vec1 += [c]
+    return (y_fit, formula3)
+
+
+# 目的関数
+def objfun(param,x,y,calc_Phi, gain=200):
+    W      = calc_W(param, x, y, gain, calc_Phi)
+    y_calc = calc_y(W, param, x, gain, calc_Phi)
+    residual = y_calc - y
+    rmse = np.sqrt((residual*residual).sum()/len(residual))
+    return rmse
+
+# 制約式
+def consfun(param):
+    cons = []
+    return cons
+
+# 重回帰分析における入力変数の情報を持つ行列の作成
+def calc_Phi_11(x, param, gain):
+    s     = sigmoid(x, param, gain)
+    ones_ = np.ones_like(x)
+    Phi = np.transpose(np.vstack([ones_*(1-s), x*(1-s), ones_*s, x*s]))
+    return Phi
+
+# 重回帰分析における入力変数の情報を持つ行列の作成
+def calc_Phi_21(x, param, gain):
+    s     = sigmoid(x, param, gain)
+    ones_ = np.ones_like(x)
+    Phi = np.transpose(np.vstack([ones_*(1-s), x*(1-s), x*x*(1-s), ones_*s, x*s]))
+    return Phi
+
+# 重回帰分析における入力変数の情報を持つ行列の作成
+def calc_Phi_12(x, param, gain):
+    s     = sigmoid(x, param, gain)
+    ones_ = np.ones_like(x)
+    Phi = np.transpose(np.vstack([ones_*(1-s), x*(1-s), ones_*s, x*s, x*x*s]))
+    return Phi
+
+# シグモイド関数
+def sigmoid(x, x0, gain):
+    return 1/(1+np.exp(-gain*(x-x0)))
+
+# 線形回帰式による出力変数の計算
+def calc_y(W, param, x, gain, calc_Phi):
+    Phi = calc_Phi(x, param, gain)
+    Y = np.dot(Phi, W)
+    return Y
+
+# リッジ回帰における未知係数の計算
+def calc_W(param, x, y, gain, calc_Phi, alpha=0.02):
+    Phi   = calc_Phi(x, param, gain)
+    Phi_T = np.transpose(Phi)
+    A = np.linalg.pinv( np.dot(Phi_T, Phi)+alpha*np.eye(Phi.shape[1]) )
+    B = np.dot(A, Phi_T)
+    W = np.dot(B, y)
+    return W
+
+
+def devided_fit(x_data, y_data, x_fit, n_deg):
+    config = read_json("config_opt.json") 
+    n_deg =(1,1)# 仮
+    if n_deg ==(1,1):
+        calc_Phi = calc_Phi_11
+    elif n_deg ==(2,1):
+        calc_Phi = calc_Phi_21
+    elif n_deg ==(1,2):
+        calc_Phi = calc_Phi_12
     
-    formula0 = [""]
-    if n_deg>=2:
-        formula0 = [coef_disp_vec1[i] + "x^{"+str(n_deg-i)+"}" for i in range(0, n_deg-1)]
+    l_param_init = np.linspace(min(x_data), max(x_data), 21)
+    fval=1e8
+    # パラメータの最適化
+    for param_init in l_param_init:
+        param_opt_, fval_, constype = nonlinopt.nonlinopt(config, param_init, (x_data,y_data,calc_Phi), objfun, consfun )
+        if fval>fval_:
+            param_opt = param_opt_
+            fval = fval_
     
-    if intercept_zero:
-        formula1 = [coef_disp_vec1[-1]+"x "]
-    else:
-        formula1 = [coef_disp_vec1[-2]+"x "+coef_disp_vec1[-1]]
+    # フィッティング
+    gain = 200
+    W_opt = calc_W(param_opt, x_data, y_data, gain, calc_Phi)
+    y_fit = calc_y(W_opt,  param_opt,  x_fit, gain, calc_Phi)
     
-    formula2 = list(filter(None, formula0+formula1))
+    # 仮
+    formula = str(W_opt)
     
-    if formula2[0].split("+")[0]=="":
-        formula2[0] = formula2[0][1:]
+    return (y_fit, formula)
+
+
+def get_plot(dataset, n_deg, intercept_zero, formula_name):
+    x_data = dataset["x"].values
+    y_data = dataset["y"].values
+    x_fit = np.linspace(min(x_data), max(x_data), 51)
     
-    formula3 = '$y = '+' '.join(formula2) + "$"
-    """
-    #import matplotlib.pyplot as plt
-    #plt.title(formula3)
-    #plt.show()
+    if formula_name=="polyfit":
+        y_fit, formula = polyfit_(x_data, y_data, x_fit, intercept_zero, n_deg)
+    elif formula_name=="devided line":
+        y_fit, formula = devided_fit(x_data, y_data, x_fit, n_deg)
     
-    
-    fig_name = "fitting_"
-    return (make_plot([x_data, x_fit], [y_data, y_fit], fig_name,\
-              marker=["o", "None"], line_style=["None", "-"], color=["k", "r"],title=formula3), formula3)
+    return (make_plot([x_data, x_fit], [y_data, y_fit], "_",\
+              marker=["o", "None"], line_style=["None", "-"], color=["k", "r"], title=formula), formula)
     
     
--- modules/optim/widget.py
+++ modules/optim/widget.py
@@ -22,7 +22,7 @@ class Widget:
     def __init__(self):
         self.DATASET_EX_NAME = ["example "+str(i+1) for i in range(3)]
         self.DATASET_EX = [pd.read_csv(name+".csv") for name in self.DATASET_EX_NAME]
-        self.FORMULA_NAME = ["polyfit", "polygonal line", "polygonal curve"]
+        self.FORMULA_NAME = ["polyfit", "devided line", "devided curve"]
         self.INTRO = _INTRO
         
         pn.config.sizing_mode = 'stretch_width'
@@ -30,7 +30,7 @@ class Widget:
         self.option.data_1    = pn.widgets.RadioBoxGroup(name= "dataset option",         options= ["example dataset", "your dataset"])
         self.option.data_2    = pn.widgets.RadioBoxGroup(name= "select example dataset", options=self.DATASET_EX_NAME)
         self.option.data_3    = pn.widgets.FileInput(name= "uploaded data", accept='.csv') # To try it with your data, upload it here
-        self.option.formula   = pn.widgets.Select(name='formula', options=self.FORMULA_NAME, disabled_options=self.FORMULA_NAME[1:])
+        self.option.formula   = pn.widgets.Select(name='formula', options=self.FORMULA_NAME)
         self.option.polyfit_deg= pn.widgets.IntSlider(name="degree of polyfit", start=0, end=10, value=1)
         self.option.intercept = pn.widgets.Checkbox(name= "fix the intercept at 0", value=False)
         self.plot = pn.pane.Matplotlib()
@@ -54,15 +54,11 @@ class Widget:
             self.dataset = pd.read_csv(buffer)
             #self.option.data_2    = pn.widgets.RadioBoxGroup(name= "select example dataset", options=["111","222","333"])
     
-    def _update_formula(self, event=None):
-        self.polyfit_deg = self.option.polyfit_deg.value
-        self.intercept_zero = self.option.intercept.value
     
     # グラフの更新
     def update_plot(self):
         self._update_dataset()
-        self._update_formula()
-        self.plot.object, _ = get_plot(self.dataset, self.polyfit_deg, self.intercept_zero)
+        self.plot.object, _ = get_plot(self.dataset, self.option.polyfit_deg.value, self.option.intercept.value, self.option.formula.value)
         #self.formula_disp.object = ""
         
 
